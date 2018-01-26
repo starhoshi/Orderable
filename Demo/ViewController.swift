f4695371d7fefad159e2afa56e2fe6688326089e
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-26 11:14:00 +0900

    setup

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let user = User()
+        user.stripeCustomerID = "test"
+        user.save()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Orderable/Order.swift
@@ -0,0 +1,20 @@
+//
+//  Order.swift
+//  Orderable
+//
+//  Created by kensuke-hoshikawa on 2018/01/26.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import Pring
+
+//extension Firebase {
+    @objcMembers
+    class User: Object {
+        /// Stripe Customer id
+        dynamic var stripeCustomerID: String?
+    }
+//}
+
