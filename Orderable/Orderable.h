b0aa14b877cdae1935a7ad8fdb01884cf062c450
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-26 10:55:44 +0900

    Initial Commit

---
+++ Orderable/Orderable.h
@@ -0,0 +1,19 @@
+//
+//  Orderable.h
+//  Orderable
+//
+//  Created by kensuke-hoshikawa on 2018/01/26.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Orderable.
+FOUNDATION_EXPORT double OrderableVersionNumber;
+
+//! Project version string for Orderable.
+FOUNDATION_EXPORT const unsigned char OrderableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Orderable/PublicHeader.h>
+
+
