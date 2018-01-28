cb4d1fc63cb8d3d84097cd1232d27015274bb162
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-28 15:57:07 +0900

    ignore

--- Pods/Target Support Files/Pods-OrderableTests/Pods-OrderableTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrderableTests : NSObject
-@end
-@implementation PodsDummy_Pods_OrderableTests
-@end
--- Pods/Target Support Files/Pring/Pring-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pring : NSObject
-@end
-@implementation PodsDummy_Pring
-@end
