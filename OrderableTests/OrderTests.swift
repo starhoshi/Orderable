083905c23313b892ae8b96ac5f99232d259d5944
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-28 22:18:39 +0900

    add ordable tests

--- OrderableTests/OrderTests.swift
+++ OrderableTests/OrderTests.swift
@@ -9,8 +9,11 @@
 import XCTest
 import Orderable
 import FirebaseCore
+import Pring
 
 class OrderableTests: XCTestCase {
+    var disposer: Disposer<Order>?
+
     var user: User?
     var shop: Shop?
     var product: Product?
@@ -22,26 +25,63 @@ class OrderableTests: XCTestCase {
     override func setUp() {
         super.setUp()
         _ = FirebaseTest.shared
-        let expectation: XCTestExpectation = XCTestExpectation(description: "save user")
+        let expectation: XCTestExpectation = XCTestExpectation(description: "order test")
 
         func fulfill() {
             if user != nil, shop != nil, product != nil, sku != nil, order != nil, orderShop != nil, orderSKU != nil {
-                expectation.fulfill()
+                order?.orderSKUs.insert(orderSKU!)
+                order?.update { [weak self] orderError in
+                    self?.orderShop?.orderSKUs.insert(self!.orderSKU!)
+                    self?.orderShop?.update { orderShopError in
+                        if orderError == nil, orderShopError == nil { expectation.fulfill() }
+                    }
+                }
             }
         }
 
         let newUser = User()
-        newUser.stripeCustomerID = "cus_test"
-        newUser.save { [weak self] ref, error in
-            self?.user = newUser
-            fulfill()
+        newUser.save { [weak self] _, _ in
+            self?.user = newUser; fulfill()
         }
+        let newShop = Shop()
+        newShop.save { [weak self] _, _ in
+            self?.shop = newShop; fulfill()
+        }
+        let newProduct = Product()
+        newProduct.save { [weak self] _, _ in
+            self?.product = newProduct; fulfill()
+        }
+        let newSKU = SKU()
+        newSKU.save { [weak self] _, _ in
+            self?.sku = newSKU; fulfill()
+        }
+        let newOrder = Order()
+        newOrder.user.set(newUser)
+        newOrder.amount = 1000
+        newOrder.save { [weak self] _, _ in
+            self?.order = newOrder; fulfill()
+        }
+        let newOrderShop = OrderShop()
+        newOrderShop.order.set(newOrder)
+        newOrderShop.user.set(newUser)
+        newOrderShop.save { [weak self] _, _ in
+            self?.orderShop = newOrderShop; fulfill()
+        }
+        let newOrderSKU = OrderSKU()
+        newOrderSKU.sku.set(newSKU)
+        newOrderSKU.shop.set(newShop)
+        newOrderSKU.snapshotSKU = newSKU
+        newOrderSKU.snapshotProduct = newProduct
+        newOrderSKU.save { [weak self] _, _ in
+            self?.orderSKU = newOrderSKU; fulfill()
+        }
+
         wait(for: [expectation], timeout: 10)
     }
 
     override func tearDown() {
         super.tearDown()
-
+        disposer = nil
         user = nil
         shop = nil
         product = nil
@@ -50,4 +90,19 @@ class OrderableTests: XCTestCase {
         orderShop = nil
         orderSKU = nil
     }
+
+    func testPayOrder() {
+        let expectation: XCTestExpectation = XCTestExpectation(description: "pay order")
+
+        order?.status = OrderStatus.paymentRequested.rawValue
+        order?.update()
+        disposer = Order.listen(order!.id) { o, e in
+            if let o = o, o.stripeChargeID != nil {
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 20)
+    }
 }
+
--- SampleModel/Model.swift
+++ SampleModel/Model.swift
@@ -12,26 +12,26 @@ import Orderable
 
 @objcMembers
 public class User: Object, UserProtocol {
-    public dynamic var stripeCustomerID: String?
+    public dynamic var stripeCustomerID: String? = "cus_CC65RZ8Gf6zi7V"
 }
 
 @objcMembers
 public class Shop: Object, ShopProtocol {
-    public dynamic var name: String?
+    public dynamic var name: String? = "shop_name"
     public dynamic var isActive: Bool = true
     public dynamic var freePostageMinimunPrice: Int = -1
 }
 
 @objcMembers
 public class Product: Object, ProductProtocol {
-    public dynamic var name: String?
+    public dynamic var name: String? = "product_name"
 }
 
 @objcMembers
 public class SKU: Object, SKUProtocol {
-    public dynamic var price: Int = 0
-    public dynamic var stockType: StockType = .unknown
-    public dynamic var stock: Int = 0
+    public dynamic var price: Int = 100
+    public dynamic var stockType: StockType = .finite
+    public dynamic var stock: Int = 100
     public dynamic var isPublished: Bool = true
     public dynamic var isActive: Bool = true
 }
@@ -44,7 +44,7 @@ class Order: Object, OrderProtocol {
     dynamic var user: Reference<OrderableUser> = .init()
 
     /// ストライプに登録したカード情報。このカードIDで決済を行う
-    dynamic var stripeCardID: String?
+    dynamic var stripeCardID: String? = "card_1BnhthKZcOra3JxsKaxABsRj"
 
     /// 総計
     dynamic var amount: Int = 0
@@ -54,12 +54,17 @@ class Order: Object, OrderProtocol {
 
     /// 有効期限 この期限を過ぎたらこのオーダーは無効になる
     dynamic var expirationDate: TimeInterval = 0
-    dynamic var stripeChargeID: String?
-    dynamic var currency: String?
-    dynamic var status: Int = OrderStatus.unknown.rawValue
+    dynamic var stripeChargeID: String? = nil
+    dynamic var currency: String? = "jpy"
+    dynamic var status: Int = OrderStatus.created.rawValue
     dynamic var orderSKUs: ReferenceCollection<OrderableOrderSKU> = []
-}
 
+    /// for Komerco
+    dynamic var skuPriceSum: Int = 0
+    dynamic var postage: Int = 0
+    dynamic var prefectureID: Int = 0
+    dynamic var regionID: Int = 0
+}
 
 @objcMembers
 class OrderShop: Object, OrderShopProtocol {
@@ -73,11 +78,10 @@ class OrderShop: Object, OrderShopProtocol {
     dynamic var orderSKUs: ReferenceCollection<OrderableOrderSKU> = []
 
     /// 配送ステータス
-    dynamic var status: Int = OrderShopStatus.unknown.rawValue
+    dynamic var status: Int = OrderShopStatus.created.rawValue
 
     /// 冗長化
     dynamic var user: Reference<OrderableUser> = .init()
-
 }
 
 @objcMembers
@@ -87,7 +91,7 @@ class OrderSKU: Object, OrderSKUProtocol {
 
     dynamic var snapshotSKU: SKUProtocol?
     dynamic var snapshotProduct: ProductProtocol?
-    dynamic var quantity: Int = 0
+    dynamic var quantity: Int = 1
     dynamic var sku: Reference<OrderableSKU> = .init()
     dynamic var shop: Reference<OrderableShop> = .init()
 }
