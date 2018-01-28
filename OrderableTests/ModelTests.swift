2b00dd2b5365a017ebcdbcce49aa51d21e2cf9e8
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-28 16:52:49 +0900

    add order test

--- OrderableTests/ModelTests.swift
+++ OrderableTests/ModelTests.swift
@@ -41,9 +41,10 @@ class ModelTests: XCTestCase {
         let expectation: XCTestExpectation = XCTestExpectation(description: "update user")
 
         let user = User()
-        user.stripeCustomerID = "cus_test_updated"
+        user.stripeCustomerID = "cus_test"
 
         user.save { ref, error in
+            user.stripeCustomerID = "cus_test_updated"
             user.update { error in
                 User.get(ref!.documentID, block: { updatedUser, error in
                     XCTAssertNotNil(updatedUser)
@@ -52,6 +53,38 @@ class ModelTests: XCTestCase {
                 })
             }
         }
+        wait(for: [expectation], timeout: 10)
+    }
+
+    func testSaveOrder() {
+        let expectation: XCTestExpectation = XCTestExpectation(description: "save order")
+
+        let user = User()
+        let order = Order()
+        order.user.set(user)
+        order.stripeCardID = "card_id"
+        order.amount = 1000
+        order.paidDate = 10
+        order.expirationDate = 100
+        order.status = .created
+        order.stripeChargeID = "charge"
+        order.currency = "jpy"
+
+        order.save { ref, error in
+            Order.get(ref!.documentID, block: { savedOrder, error in
+                XCTAssertNotNil(savedOrder)
+                XCTAssertEqual(savedOrder?.user.id, user.id)
+                XCTAssertEqual(savedOrder?.stripeCardID, "card_id")
+                XCTAssertEqual(savedOrder?.amount, 1000)
+                XCTAssertEqual(savedOrder?.paidDate, 10)
+                XCTAssertEqual(savedOrder?.expirationDate, 100)
+                XCTAssertEqual(savedOrder?.status, .created)
+                XCTAssertEqual(savedOrder?.stripeChargeID, "charge")
+                XCTAssertEqual(savedOrder?.currency, "jpy")
+                expectation.fulfill()
+            })
+        }
+
         wait(for: [expectation], timeout: 10)
     }
 }
